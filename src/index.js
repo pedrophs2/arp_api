ec6c33a830cbddf8c5e5f721efd829b77745f689
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-02-04 20:42:33 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const UserRoutes = require('./routes/UserRoutes')
 
 server.use('/user', UserRoutes)
 
+server.get('/', (req, res) => {
+    res.send('<h1>Working...</h1>')
+})
+
 server.listen(3000, () => {
     console.log('API Online (With Nodemon) !!')
 })
