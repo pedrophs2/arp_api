fa68e32816227271dd161449f0de3a2c8caf9418
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-02-04 20:47:39 -0300

    Index Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
 server.use(express.json())
 
-const UserRoutes = require('./routes/UserRoutes')
+const UserRoutes = require('./src/routes/UserRoutes')
 
 server.use('/user', UserRoutes)
 
