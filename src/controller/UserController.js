4bf7395bf679c5792e2fd536fa40846fc6f0c572
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-02-04 21:05:10 -0300

    List Users

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -3,11 +3,21 @@ const UserModel = require('../model/UserModel')
 class UserController {
     
     async create(req, res) {
-        const task = new UserModel(req.body)
+        const user = new UserModel(req.body)
         
-        await task.save()
+        await user.save()
         .then( response => {
             return res.status(200).json(response)
+        })
+        .catch( error => {
+            return res.status(500).json(error)
+        })
+    }
+
+    async list(req, res) {
+        await UserModel.find()
+        .then( response => {
+            return res.status(2002).json(response)
         })
         .catch( error => {
             return res.status(500).json(error)
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const UserController = require('../controller/UserController')
 
 router.post('/', UserController.create)
+router.get('/', UserController.list)
 
 module.exports = router
