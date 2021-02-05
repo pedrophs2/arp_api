556674e4bc18a729dfb76c57d3cb070df8c37304
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-02-04 22:30:34 -0300

    Update index.js
    **GENERAL
    Updated console log and index message

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const UserRoutes = require('./src/routes/UserRoutes')
 server.use('/user', UserRoutes)
 
 server.get('/', (req, res) => {
-    res.send('<h1>Working...</h1>')
+    res.send('<h1>Working ! Only API for now...</h1>')
 })
 
 server.listen(3000, () => {
-    console.log('API Online (With Nodemon) !!')
+    console.log('API Online !!')
 })
