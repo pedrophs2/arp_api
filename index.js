61120581125b10a8f1d2eec9f92d950bef3b4d2f
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-02-05 11:16:19 -0300

    Added GitHub
    ## GENERAL
    Added GitHub Link to Main page

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const UserRoutes = require('./src/routes/bck/UserRoutes')
 server.use('/api/bck/user', UserRoutes)
 
 server.get('/', (req, res) => {
-    res.send('<h1>Working ! Only API for now...</h1>')
+    res.send(
+        '<h1>Working ! Only API for now...</h1>' +
+        '<br>' + 
+        'Check our GitHub Documentation: <a href="https://github.com/pedrophs2/arp_api">Click Here !</a>'
+        )
 })
 
 server.listen(3000, () => {
