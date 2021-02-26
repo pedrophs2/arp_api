86bce8358bcf9498d363088c8d399caaf8d00511
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-02-26 18:46:35 -0300

    Token Change

--- src/routes/mct/OrcamentoRoutes.js
+++ src/routes/mct/OrcamentoRoutes.js
@@ -13,7 +13,7 @@ router.put('/:id', verifyJWT, OrcamentoController.updateOrcamento)
 router.delete('/:id', verifyJWT, OrcamentoController.deleteOrcamento)
 
 function verifyJWT(req, res, next) {
-    const token = req.headers['Authorization']
+    const token = req.headers.authorization
     jwt.verify(token, hash, (err, decoded) => {
         if(err) return res.status(401).send({error: 'Token Inválido ou Ausente'})
         next()
