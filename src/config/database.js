dfacdac30a9305d10de668555f97a44014d3d90c
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-02-25 23:25:31 -0300

    Update database.js
    Fix connection issues

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 async function connect() {
-    if(global.connection && global.connection.state !== 'disconnected')
-        return global.connection
+    if(global.connection != undefined && global.connection.state !== 'disconnected')
+        global.connection.end()
 
     const mysql = require('mysql2/promise')
     const conn = await  mysql.createConnection('mysql://arp-master:arpmaster2021@mysql742.umbler.com:41890/arp-api-msql')
