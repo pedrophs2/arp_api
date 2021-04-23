442aaa6dd8f04babaf72be2f65b0956768789256
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-04-23 19:05:48 -0300

    WEB PROJ RUNNING

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 
 import routes from './routes';
+import path from 'path'
 
 const server = express();
 
@@ -10,6 +11,11 @@ server.use(express.json());
 server.use(cors())
 server.use(bodyParser.json())
 server.use(routes);
+server.use('/public', express.static(`${__dirname}/dist/frontend`));
+
+server.get('/home', async (req: any, res: any) => {
+    res.sendFile(path.join(`${__dirname}/dist/frontend/index.html`))
+})
 
 server.listen(3000, () => {
     console.log('API ONLINE => http://arpdevs.com.br/api/\'project\'/\'class\'/\'endpoint\' (With TypeScript)')
