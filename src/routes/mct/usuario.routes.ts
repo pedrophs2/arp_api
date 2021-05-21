a06e5ca566307e1e8b56cf68032d409349a2fa4e
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-05-21 08:54:28 -0300

    Swagger Init
    Swagger Annotions

--- src/routes/mct/usuario.routes.ts
+++ src/routes/mct/usuario.routes.ts
@@ -4,8 +4,87 @@ import JwtChecker from '../../middleware/JwtChecker'
 
 const usuarioRoutes = Router();
 
+/**
+ * @swagger
+ * api/mct/usuarios:
+ *      get: 
+ *          summary: Listagem de todos os usuários cadastrados
+ *          description: Listagem de todos os usuários cadastrados
+ *          produces: application/json
+ *          responses:
+ *              '200':
+ *                  description: Usuários encontrados
+ *              '400': 
+ *                  description: Usuários não encontrados
+ */
 usuarioRoutes.get('/', JwtChecker.verifyJwt, UsuarioController.listUsers)
+
+/**
+ * @swagger
+ * api/mct/usuarios/{idUsuario}:
+ *      get: 
+ *          summary: Exibição de Usuário
+ *          description: Exibição de usuário selecionado
+ *          produces: 
+ *              - application/json
+ *          parameters: 
+ *              - name: idUsuario
+ *                required: true
+ *                type: string
+ *          responses:
+ *              '200':
+ *                  description: Usuário encontrado
+ *              '400': 
+ *                  description: Usuário não encontrado
+ */
 usuarioRoutes.get('/:id', JwtChecker.verifyJwt, UsuarioController.getUser)
+
+/**
+ * @swagger
+ * api/mct/usuarios:
+ *      post: 
+ *          summary: Criação de usuário
+ *          description: Criação de usuário
+ *          responseClass: Usuário
+ *          consumes: application/json
+ *          parameters: 
+ *              - name: usuario_cpf
+ *                required: true
+ *                type: string
+ *                example: 111.111.111-11
+ *              - name: usuario_nome
+ *                required: true
+ *                type: string
+ *                example: Fulano da Silva
+ *              - name: usuario_email
+ *                required: true
+ *                type: string
+ *                example: fulano@email.com
+ *              - name: usuario_senha
+ *                required: true
+ *                type: string
+ *              - name: usuario_fone
+ *                required: true
+ *                type: string
+ *                example: (77) 99999-999
+ *              - name: usuario_orcamentos
+ *                required: true
+ *                type: integer
+ *                example: 0
+ *              - name: usuario_vip
+ *                required: true
+ *                type: boolean
+ *                example: true
+ *              - name: usuario_vip_dt_final
+ *                required: true
+ *                type: timestamp
+ *                
+ *          responses:
+ *              '200':
+ *                  description: Usuário criado
+ *              '400': 
+ *                  description: Usuário não criado
+ */
 usuarioRoutes.post('/', JwtChecker.verifyJwt, UsuarioController.createUser)
 usuarioRoutes.put('/:id', JwtChecker.verifyJwt, UsuarioController.updateUser)
 usuarioRoutes.delete('/:id', JwtChecker.verifyJwt, UsuarioController.deleteUser)
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,27 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import routes from './routes';
 import path from 'path'
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+//Swagger Configuration - Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'ArpAPI',
+            description: 'API de testes para projetos da ArpDevs',
+            contact: {
+                name: 'ArpDevs'
+            },
+            servers: ['http://localhost:3000/', 'http://arpdevs.com.br/'],
+            version: '1.0.0'
+        }
+    },
+    apis: ['**/*.ts']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+
 
 //Server Startup
 const server = express();
@@ -17,13 +38,16 @@ server.use(routes);
 server.use('/public', express.static(`${__dirname}/dist/frontend`));
 server.use('/csn/public', express.static(`${__dirname}/dist/casa-nova`));
 
-server.get('/mct/admin', async (req: any, res: any) => {
-    res.sendFile(path.join(`${__dirname}/dist/frontend/index.html`))
-})
+//Swagger Route
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
-server.get('/casanova', async (req: any, res: any) => {
-    res.sendFile(path.join(`${__dirname}/dist/casa-nova/index.html`))
-})
+// server.get('/mct/admin', async (req: any, res: any) => {
+//     res.sendFile(path.join(`${__dirname}/dist/frontend/index.html`))
+// })
+
+// server.get('/casanova', async (req: any, res: any) => {
+//     res.sendFile(path.join(`${__dirname}/dist/casa-nova/index.html`))
+// })
 
 server.listen(3000, () => {
     console.log('API ONLINE => http://arpdevs.com.br/api/\'project\'/\'class\'/\'endpoint\'')
