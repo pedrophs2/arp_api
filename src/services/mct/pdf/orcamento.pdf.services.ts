02a27c7729a6040d655c9a2a45ecabbd8ab01fd9
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-06-29 07:50:25 -0300

    Update orcamento.pdf.services.ts

--- src/services/mct/pdf/orcamento.pdf.services.ts
+++ src/services/mct/pdf/orcamento.pdf.services.ts
@@ -17,35 +17,36 @@ const options: pdf.CreateOptions = {
 class OrcamentoPDFServices {
 
     async generatePDFOrcamento(req: any, res: any) {
-        let orcamento = req.body
-        console.log(orcamento)
+        try {
+            let orcamento = req.body
+            orcamentoHtml = orcamentoHtml.toString()
 
-        orcamentoHtml = orcamentoHtml.toString()
+            // Cliente
+            orcamentoHtml = orcamentoHtml.replace('{{cliente_nome}}', orcamento.orcamento_cliente.cliente_nome)
+            orcamentoHtml = orcamentoHtml.replace('{{cliente_nome}}', orcamento.orcamento_cliente.cliente_nome)
+            orcamentoHtml = orcamentoHtml.replace('{{cliente_endereco}}', orcamento.orcamento_cliente.cliente_endereco)
+            orcamentoHtml = orcamentoHtml.replace('{{cliente_fone}}', orcamento.orcamento_cliente.cliente_fone)
 
-        // Cliente
-        orcamentoHtml = orcamentoHtml.replace('{{cliente_nome}}', orcamento.orcamento_cliente.cliente_nome)
-        orcamentoHtml = orcamentoHtml.replace('{{cliente_nome}}', orcamento.orcamento_cliente.cliente_nome)
-        orcamentoHtml = orcamentoHtml.replace('{{cliente_endereco}}', orcamento.orcamento_cliente.cliente_endereco)
-        orcamentoHtml = orcamentoHtml.replace('{{cliente_fone}}', orcamento.orcamento_cliente.cliente_fone)
+            // Orcamento
+            orcamentoHtml = orcamentoHtml.replace('{{orcamento_id}}', orcamento.orcamento_id || 'Não salvo')
+            orcamentoHtml = orcamentoHtml.replace('{{orcamento_data}}', moment(orcamento.orcamento_data).format('DD/MM/YYYY'))
+            orcamentoHtml = orcamentoHtml.replace('{{orcamento_nome}}', orcamento.orcamento_nome)
+            orcamentoHtml = orcamentoHtml.replace('{{orcamento_total}}', orcamento.orcamento_total)
+            orcamentoHtml = orcamentoHtml.replace('{{orcamento_total}}', orcamento.orcamento_total)
+            orcamentoHtml = orcamentoHtml.replace('{{orcamento_valor_final}}', orcamento.orcamento_valor_final)
 
-        // Orcamento
-        orcamentoHtml = orcamentoHtml.replace('{{orcamento_id}}', orcamento.orcamento_id || 'Não salvo')
-        orcamentoHtml = orcamentoHtml.replace('{{orcamento_data}}', moment(orcamento.orcamento_data).format('DD/MM/YYYY'))
-        orcamentoHtml = orcamentoHtml.replace('{{orcamento_nome}}', orcamento.orcamento_nome)
-        orcamentoHtml = orcamentoHtml.replace('{{orcamento_total}}', orcamento.orcamento_total)
-        orcamentoHtml = orcamentoHtml.replace('{{orcamento_total}}', orcamento.orcamento_total)
-        orcamentoHtml = orcamentoHtml.replace('{{orcamento_valor_final}}', orcamento.orcamento_valor_final)
+            pdf.create(orcamentoHtml, options).toBuffer((err: Error, buffer: Buffer) => {
+                if (err) return res.status(500).json(err)
 
-        pdf.create(orcamentoHtml, options).toBuffer((err: Error, buffer: Buffer) => {
-            if(err) return res.status(500).json(err)
-
-            res.end(buffer)
-        })
+                res.end(buffer)
+            })
+        } catch(error) {
+            console.log(error)
+        }
     }
 
     async generatePDFOrcamentoSimplificadoFile(req: any, res: any) {
         let orcamento = req.body
-        console.log(orcamento)
 
         orcamentoHtml = orcamentoHtml.toString()
         const fileName = this.mountFileName(orcamento)
@@ -65,8 +66,8 @@ class OrcamentoPDFServices {
         orcamentoHtml = orcamentoHtml.replace('{{orcamento_total}}', orcamento.orcamento_total)
         orcamentoHtml = orcamentoHtml.replace('{{orcamento_valor_final}}', orcamento.orcamento_valor_final)
 
-        pdf.create(orcamentoHtml, options).toFile(`${filename.replace('orcamento-resumido', `${fileName}`)}`,(err: Error, pdf: pdf.FileInfo) => {
-            if(err) return res.status(500).json(err)
+        pdf.create(orcamentoHtml, options).toFile(`${filename.replace('orcamento-resumido', `${fileName}`)}`, (err: Error, pdf: pdf.FileInfo) => {
+            if (err) return res.status(500).json(err)
 
             fs.existsSync(pdf.filename)
             res.end(pdf.filename)
