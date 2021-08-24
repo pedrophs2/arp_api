c320a62ecb890208764ca291029445ef2f5a4651
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-08-24 08:37:05 -0300

    Update services.ts

--- src/third_party/images/cloudinary/services.ts
+++ src/third_party/images/cloudinary/services.ts
@@ -9,7 +9,7 @@ class CloudinaryProvider implements ImageProvider {
     async uploadImage(title: string, image: string, path?: string): Promise<string> {
         try {
             service.v2.config(credentials)
-            let data = await service.v2.uploader.upload(image, { folder: path, public_id: title, overwrite: true })
+            let data = await service.v2.uploader.upload(image, { folder: path, public_id: title, overwrite: true, unique_filename: true, transformation: {height: 540} })
             return data.url
         } catch(error) {
             console.log(error)
