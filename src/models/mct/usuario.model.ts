20214bb72ab8067fad0d2dd7f05f12ed3ebd809e
Pedro Henrique <38799159+pedrophs2@users.noreply.github.com>
2021-08-24 21:27:29 -0300

    Update usuario.model.ts

--- src/models/mct/usuario.model.ts
+++ src/models/mct/usuario.model.ts
@@ -7,6 +7,7 @@ export class Usuario {
     usuario_orcamentos: number
     usuario_vip: boolean
     usuario_vip_dt_final: string
+    usuario_logo: string
 
     constructor(
         usuario_cpf?: string,
@@ -16,7 +17,8 @@ export class Usuario {
         usuario_fone?: string,
         usuario_orcamentos?: number,
         usuario_vip?: boolean,
-        usuario_vip_dt_final?: string
+        usuario_vip_dt_final?: string,
+        usuario_logo?: string
     ) {
         this.usuario_cpf = usuario_cpf
         this.usuario_nome = usuario_nome
@@ -26,5 +28,6 @@ export class Usuario {
         this.usuario_orcamentos = usuario_orcamentos
         this.usuario_vip = usuario_vip
         this.usuario_vip_dt_final = usuario_vip_dt_final
+        this.usuario_logo = usuario_logo
     }
 }
