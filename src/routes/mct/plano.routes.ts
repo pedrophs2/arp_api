16edc2d18d084bdea458b10f5e5db0cf9821b976
Pedro Henrique <38799159+pedrophs2@users.noreply.github.com>
2021-11-28 14:14:43 -0300

    Add Routes

--- src/routes/mct/plano.routes.ts
+++ src/routes/mct/plano.routes.ts
@@ -7,5 +7,7 @@ const router = Router()
 
 router.get('/', JwtChecker.verifyJwt, PlanoController.listPlanos)
 router.get('/:plano_id', JwtChecker.verifyJwt, PlanoController.getPlano)
+router.post('/', JwtChecker.verifyJwt, PlanoController.createPlano)
+router.put('/:plano_id', JwtChecker.verifyJwt, PlanoController.updatePlano)
 
 export default router
